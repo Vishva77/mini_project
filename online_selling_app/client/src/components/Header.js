2717b07671289a2ac6049ecffc8925c0c348d901
Vishva77 <vishvavikki77@gmail.com>
2024-04-13 15:14:44 +0530

    some changes in selling app

--- online_selling_app/client/src/components/Header.js
+++ online_selling_app/client/src/components/Header.js
@@ -4,18 +4,28 @@ import Table from "./Table.js";
 import "./Header.css";
 
 function Header() {
-  const [form, SetForm] = useState(false);
-  const [table, SetTable] = useState(false);
+  const [form, setForm] = useState(false);
+  const [table, setTable] = useState(false);
+
+  const toggleForm = () => {
+    setForm(true);
+    setTable(false);
+  };
+
+  const toggleTable = () => {
+    setTable(true);
+    setForm(false);
+  };
 
   return (
     <div className="Header">
       <h1>Online Selling Platform</h1>
       <div className='btn'>
-        <button onClick={() =>{SetForm(!form)}}>SELL</button>
-        <button onClick={() =>{SetTable(!table)}}>BUY</button>
+        <button onClick={toggleForm}>SELL</button>
+        <button onClick={toggleTable}>BUY</button>
       </div>
-      {form && <Form/>}
-      {table && <Table/>}
+      {form && <Form />}
+      {table && <Table />}
     </div>
   );
 }
